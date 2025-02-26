300704e1472f75008019de57b951ef9a15d329b3
StepanKrivosheev <stepan.krivosheev@smail.th-koeln.de>
2025-02-26 21:16:30 +0100

    test

--- src/app/(studio)/studio/[[...tool]]/page.tsx
+++ src/app/(studio)/studio/[[...tool]]/page.tsx
@@ -1,23 +1,34 @@
-/**
- * This route is responsible for the built-in authoring environment using Sanity Studio.
- * All routes under your studio path is handled by this file using Next.js' catch-all routes:
- * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
- *
- * You can learn more about the next-sanity package here:
- * https://github.com/sanity-io/next-sanity
- */
+'use client'
 
-import { NextStudio } from "next-sanity/studio";
-import config from "../../../../../sanity.config";
+import {NextStudio} from 'next-sanity/studio'
 
-export const dynamic = "force-static";
+import config from '@/sanity.config'
 
-export { metadata, viewport } from "next-sanity/studio";
-
-// export async function generateStaticParams() {
-// 	return {};
-// }
-
-export default function StudioPage() {
-	return <NextStudio config={config} />;
+export function Studio() {
+  //  Supports the same props as import {Studio} from 'sanity', config is required
+  return <NextStudio config={config} />
 }
+
+// /**
+//  * This route is responsible for the built-in authoring environment using Sanity Studio.
+//  * All routes under your studio path is handled by this file using Next.js' catch-all routes:
+//  * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
+//  *
+//  * You can learn more about the next-sanity package here:
+//  * https://github.com/sanity-io/next-sanity
+//  */
+
+// import { NextStudio } from "next-sanity/studio";
+// import config from "../../../../../sanity.config";
+
+// export const dynamic = "force-static";
+
+// // export { metadata, viewport } from "next-sanity/studio";
+
+// // export async function generateStaticParams() {
+// // 	return {};
+// // }
+
+// export function StudioPage() {
+// 	return <NextStudio config={config} />;
+// }
