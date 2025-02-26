195738642b65759d3a671b668360c52026bc4583
StepanKrivosheev <stepan.krivosheev@smail.th-koeln.de>
2025-02-26 21:21:51 +0100

    maybe

--- src/app/(studio)/studio/[[...tool]]/page.tsx
+++ src/app/(studio)/studio/[[...tool]]/page.tsx
@@ -1,12 +1,12 @@
-'use client'
+"use client";
 
-import {NextStudio} from 'next-sanity/studio'
+import { NextStudio } from "next-sanity/studio";
 
-import config from '@/sanity.config'
+import config from "@/sanity.config";
 
-export function StudioPage() {
-  //  Supports the same props as import {Studio} from 'sanity', config is required
-  return <NextStudio config={config} />
+export default function StudioPage() {
+	//  Supports the same props as import {Studio} from 'sanity', config is required
+	return <NextStudio config={config} />;
 }
 
 // /**
