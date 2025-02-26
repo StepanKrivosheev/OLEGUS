2113ce3929190ebed5309d904f08b8112001f79f
StepanKrivosheev <stepan.krivosheev@smail.th-koeln.de>
2025-02-26 20:28:32 +0100

    test

--- src/app/(studio)/studio/[[...tool]]/page.tsx
+++ src/app/(studio)/studio/[[...tool]]/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 /**
  * This route is responsible for the built-in authoring environment using Sanity Studio.
  * All routes under your studio path is handled by this file using Next.js' catch-all routes:
@@ -11,10 +10,9 @@
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../../../sanity.config";
 
-// export const dynamic = "force-static";
+export const dynamic = "force-static";
 
-// export { metadata, viewport } from "next-sanity/studio";
-
+export { metadata, viewport } from "next-sanity/studio";
 
 export default function StudioPage() {
 	return (
