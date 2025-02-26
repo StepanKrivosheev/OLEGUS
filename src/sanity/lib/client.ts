d4b7549b855bda48f1ba5494a9e03db057c384d8
StepanKrivosheev <stepan.krivosheev@smail.th-koeln.de>
2025-02-26 19:11:32 +0100

    fix

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -2,6 +2,7 @@ import { createClient } from 'next-sanity'
 
 import { apiVersion, dataset, projectId } from '../env'
 
+console.log(projectId);
 export const client = createClient({
   projectId,
   dataset,
