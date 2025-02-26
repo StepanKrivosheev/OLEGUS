6e34216af6324b2e727cb256d84ecd2edaae8208
StepanKrivosheev <stepan.krivosheev@smail.th-koeln.de>
2025-02-26 21:38:52 +0100

    test

--- src/app/(studio)/layout.tsx
+++ src/app/(studio)/layout.tsx
@@ -3,11 +3,6 @@ import type { Metadata } from "next";
 import "@/src/app/(root)/globals.css";
 import React from "react";
 
-export const metadata: Metadata = {
-	title: "Oleg und Söhne",
-	description: "Owner Oleg Krivosheev, designed by Stepan Krivosheev",
-};
-
 export default function RootLayout({
 	children,
 }: {
