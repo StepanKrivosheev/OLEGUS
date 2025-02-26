a290d3ee84bcf3e49a0e7b60d8cffb50a32c06d1
StepanKrivosheev <stepan.krivosheev@smail.th-koeln.de>
2025-02-26 19:43:47 +0100

    test

--- src/app/(studio)/studio/[[...tool]]/page.tsx
+++ src/app/(studio)/studio/[[...tool]]/page.tsx
@@ -17,3 +17,7 @@ export { metadata, viewport } from "next-sanity/studio";
 export default function StudioPage() {
 	return <NextStudio config={config} />;
 }
+
+export async function generateStaticParams() {
+	return [];
+}
