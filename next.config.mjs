9e268c500f2dd8bd09b80e0add8a8705d2f1d849
StepanKrivosheev <stepan.krivosheev@smail.th-koeln.de>
2025-02-27 18:45:34 +0100

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: "cdn.sanity.io",
-      },
-    ],
-  },}
+const nextConfig = {   
+  env: {
+    DISABLE_SERVER_ACTIONS: process.env.NODE_ENV === 'production',
+    }, 
+  images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: "cdn.sanity.io",
+        },
+      ],
+    },}
 
 export default nextConfig;
