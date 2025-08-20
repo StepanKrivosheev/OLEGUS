2370999743aaf4af146c8365d573fd33ee2181be
StepanKrivosheev <stepan.krivosheev@smail.th-koeln.de>
2025-08-20 14:26:59 +0200

    fixed gallery

--- src/sanity/lib/queryies.ts
+++ src/sanity/lib/queryies.ts
@@ -1,29 +1,29 @@
 import { defineQuery } from "next-sanity";
 
 export const PROJECT_QUERY = defineQuery(
-	'*[_type == "project"] | order(_createdAt desc) '
+	'*[_type == "project"] | order(date desc) '
 );
 
 export const IMAGES_QUERY = defineQuery(
-	'*[_type == "project"]{"images": Images[0]}'
+	'*[_type == "project"]{"images": Images}'
 );
 
 export const IMAGES_FOR_GALLERY_QUERY = defineQuery(
-	'*[_type == "project"]{ImagesForGallery}'
+	'*[_type == "project"]{"images" : ImagesForGallery}'
 );
 
 export const BATHROOM_QUERY = defineQuery(
-	'*[_type == "project" && Category == "bathroom" ] | order(_createdAt desc)'
+	'*[_type == "project" && Category == "bathroom" ] | order(date desc)'
 );
 
 export const LIVINGROOM_QUERY = defineQuery(
-	'*[_type == "project" && Category == Livingroom] | order(_createdAt desc) '
+	'*[_type == "project" && Category == Livingroom] | order(date desc) '
 );
 
 export const KITCHEN_QUERY = defineQuery(
-	'*[_type == "project" && Category == Kitchen] | order(_createdAt desc) '
+	'*[_type == "project" && Category == Kitchen] | order(date desc) '
 );
 
 export const SAUNA_QUERY = defineQuery(
-	'*[_type == "project" && Category == Sauna] | order(_createdAt desc) '
+	'*[_type == "project" && Category == Sauna] | order(date desc) '
 );
